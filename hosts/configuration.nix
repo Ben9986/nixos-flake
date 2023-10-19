f9f36489858f47379b9839d9be567f12c471d951
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2023-10-20 00:30:14 +0100

    remove mess from configuration.nix

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -3,38 +3,6 @@
 # and in the NixOS manual (accessible by running `nixos-help`).
 
 { inputs, config, lib, pkgs, ... }:
-let
- #  zenbook-acpi = pkgs.stdenv.mkDerivation rec {
- #    pname = "asus-zenbook-ux3402za-acpi-tables";
- #    version = "0.0.1";
- #    src = pkgs.fetchFromGitHub {
- #      owner = "thor2002ro";
- #      repo = "asus_zenbook_ux3402za";
- #      rev = "54cc1488e8dd2ffbf3dbffd390e42d9a4657a2de";
- #      sha256 = "sha256-o32kAz8z/AMsKfCmU0nHxhcerDudYqJduHhRCeZbVPQ=";
- #    };
-
- #    nativeBuildInputs = with pkgs; [
- #      acpica-tools
- #    ];
-
- #    buildPhase = ''
- #      cp -r Sound Work
- #      iasl -tc Work/ssdt-csc3551.dsl
- #    '';
-
- #    installPhase = ''
- #      mkdir -p $out/boot $out/etc/grub.d
- #      cp Work/ssdt-csc3551.aml $out/boot/ssdt-csc3551.aml
- #    '';
- #  };
-
- #  flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
-
- #  hyprland-flake = (import flake-compat {
- #    src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/master.tar.gz";
- #  }).defaultNix;
-in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -66,69 +34,10 @@ in
       '';
     settings = {
       auto-optimise-store = true;
-      # substituters = ["https://hyprland.cachix.org"];
-      #trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
     package = pkgs.nixFlakes;
   };
   
-  # Use the systemd-boot EFI boot loader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.systemd-boot.configurationLimit = 10;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  
- #  boot = {
- #    loader.efi.canTouchEfiVariables = true;
- #    loader.grub = {
- #      enable = true;
- #      device = "nodev";
- #      efiSupport = true;
- #      # useOSProber = true;
- #      extraEntriesBeforeNixOS = true;
- #      extraEntries = ''
- #         menuentry 'Windows Boot Manager (on /dev/nvme0n1p3)' --class windows --class os $menuentry_id_option 'osprober-efi-89A9-5B80' {
- #           insmod part_gpt
- #           insmod fat
- #           search --no-floppy --fs-uuid --set=root 89A9-5B80
- #           chainloader /EFI/Microsoft/Boot/bootmgfw.efi
- #         }
- #      '';
- #      # gfxmodeEfi = "2880x1800";
- #      timeoutStyle = "hidden";
- #      theme = pkgs.stdenv.mkDerivation {
- #        name = "vimix-grub-theme";
- #        buildInputs = [ pkgs.bash ];
- #  	src = pkgs.fetchFromGitHub {
- #    	  owner = "vinceliuice";
- #    	  repo = "grub2-themes";
- #    	  rev = "2022-10-30";
- #    	  hash = "sha256-LBYYlRBmsWzmFt8kgfxiKvkb5sdLJ4Y5sy4hd+lWR70=";
- #  	};
- #        installPhase = ''
- #        var="#! ${pkgs.bash}/bin/bash"
- #        echo $var
- #        sed -i "1c\$var" install.sh
- #        chmod +x ./install.sh
- #        ./install.sh -t vimix -s 2k -g $out
- #        mv $out/vimix/* $out
- #        rm -r $out/vimix
- #        '';
-}# ;
- #      backgroundColor = "#000000";
- #      extraFiles."ssdt-csc3551.aml" = "${zenbook-acpi}/boot/ssdt-csc3551.aml";
- #      extraConfig = "acpi ($root)/ssdt-csc3551.aml";
- #    };
- #    kernelParams = ["quiet"];
- #    plymouth = {
- #      enable = true;
- #      theme = "catppuccin-mocha";
- #      themePackages = [(pkgs.catppuccin-plymouth.override {variant = "mocha";})];
- #      # themePackages = [(pkgs.adi1090x-plymouth-themes.override {selected_themes = ["double"];})];
- #    };
- #    consoleLogLevel = 1;
- #  };
-
-
 
   networking.hostName = "benlaptop";
   networking.networkmanager.enable = true;
@@ -186,31 +95,13 @@ in
 	 #logind-check-graphical=true
       '';
     };
-    # desktopManager.cinnamon.enable = true;
 
   };
   
-  # xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   
   systemd.services."display-manager".preStart = "sleep 5";
- #  systemd.user = {
- #    services.polkit-gnome-authentication-agent-1 = {
- #      description = "polkit-gnome-authentication-agent-1";
- #      wantedBy = [ "graphical-session.target" ];
- #      wants = [ "graphical-session.target" ];
- #      after = [ "graphical-session.target" ];
- #      serviceConfig = {
- #          Type = "simple";
- #          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
- #          Restart = "yes";
- #          RestartSec = 1;
- #          TimeoutStopSec = 10;
- #      };
- #    };
- #  };
-
-
+ 
   security = {
     polkit.enable = true;
     pam.services.swaylock = {};
