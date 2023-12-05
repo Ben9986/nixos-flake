3bd89f549dc482865534a1e9d0d84d8a45e74f24
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2023-12-05 23:24:48 +0000

    hm: remove duplicate polkit service

--- home-manager/ben.nix
+++ home-manager/ben.nix
@@ -32,23 +32,6 @@
     };
   };
 
-  systemd.user.services.polkit-gnome-authentication-agent-1 = {
-  Unit = {
-    Description = "Gnome Polkit authentication agent";
-    Documentation = "https://gitlab.freedesktop.org/polkit/polkit/";
-    After = [ "graphical-session-pre.target" ];
-    PartOf = [ "graphical-session.target" ];
-  };
-
-  Service = {
-    ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-    Restart = "always";
-    BusName = "org.freedesktop.PolicyKit1.Authority";
-  };
-
-  Install.WantedBy = [ "graphical-session.target" ];
-};
-
   home.stateVersion = "23.05"; # Please read the comment before changing.
   
   dconf = {
