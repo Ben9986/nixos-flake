5c287667dcd6f8bd657d943ca08622a45174119d
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2023-12-11 20:35:38 +0000

    hypr: fix hyprpaper config path

--- hosts/bendesktop/hyprland.nix
+++ hosts/bendesktop/hyprland.nix
@@ -1,5 +1,5 @@
 {...}:        
 { 
 wayland.windowManager.hyprland.settings.monitor = "HDMI-A-1,highres,auto,1";        
-home.file.".config/hypr/hyprpaper.conf".source = ../../home-manager/dotfiles/hypr/hyprpaper-.conf;
+home.file.".config/hypr/hyprpaper.conf".source = ../../home-manager/dotfiles/hypr/hyprpaper-desktop.conf;
 }
