8655db096387aa245f59a417c7d35e7a176c916f
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2023-12-12 23:34:33 +0000

    fix more paths...

--- modules/home-manager/hyprland-laptop.nix
+++ modules/home-manager/hyprland-laptop.nix
@@ -1,5 +1,5 @@
 {...}:        
 { 
 wayland.windowManager.hyprland.settings.monitor = "eDP-1,highres,0x0,1.75";        
-home.file.".config/hypr/hyprpaper.conf".source = ../../home-manager/dotfiles/hypr/hyprpaper-laptop.conf;
+home.file.".config/hypr/hyprpaper.conf".source = ./dotfiles/hypr/hyprpaper-laptop.conf;
 }
