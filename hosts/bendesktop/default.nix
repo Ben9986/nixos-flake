3d8fb3f8b8ef8178e292818961747570025a3f51
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2023-12-26 22:01:33 +0000

    enable bluetooth on desktop

--- hosts/bendesktop/default.nix
+++ hosts/bendesktop/default.nix
@@ -61,4 +61,9 @@
  
   networking.hostName = "bendesktop";
 
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  services.blueman.enable = true;
+
 }
