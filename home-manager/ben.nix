c3f963995e1ef0247e3a94fec05a360441e39bf8
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-01-04 17:47:16 +0000

    nix: pull flake changes on login

--- home-manager/ben.nix
+++ home-manager/ben.nix
@@ -38,6 +38,34 @@
     };
 };
 
+  systemd.user.timers."pull-flake" = {
+    Install = {
+      WantedBy =  [ "timers.target" ];
+    };
+    Timer = {
+      OnStartupSec = "20";
+      Unit = "pull-flake.service";
+    };
+  };
+ 
+  systemd.user.services."pull-flake" = {
+     Unit = {
+      Description = "Pull changes for flake configuration from github";
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.writeShellScript "pull-flake" ''
+      set -eu
+      ${pkgs.coreutils}/bin/echo "Starting Git Pull..."
+      cd /etc/nixos/
+      ${pkgs.libnotify}/bin/notify-send "Checking for Config Changes..." "$(${pkgs.git}/bin/git pull)" -t 4000 -e
+    ''}";
+      Type = "oneshot";
+      };
+  };
+
   home.stateVersion = "23.05"; # Please read the comment before changing.
   
   dconf = {
