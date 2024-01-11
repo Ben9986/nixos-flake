3678ec51c700d39caaebf6d1eea685d4c439b4d0
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-01-11 22:11:58 +0000

    hypr: set laptop scale to 1.8

--- home-manager/modules/hyprland-laptop.nix
+++ home-manager/modules/hyprland-laptop.nix
@@ -1,5 +1,5 @@
 {...}:        
 { 
-wayland.windowManager.hyprland.settings.monitor = "eDP-1,highres,0x0,1.75";        
+wayland.windowManager.hyprland.settings.monitor = "eDP-1,highres,0x0,1.8";        
 home.file.".config/hypr/hyprpaper.conf".source = ../dotfiles/hypr/hyprpaper-laptop.conf;
 }
