6a4e1ed79dc201563f8246aa7c1744ccc48d3bc1
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-04-23 16:44:55 +0100

    hypr: desktop nvidia envs for minecraft

--- home-manager/modules/hyprland-desktop.nix
+++ home-manager/modules/hyprland-desktop.nix
@@ -4,6 +4,10 @@ wayland.windowManager.hyprland.settings = {
   monitor = "HDMI-A-1,highres,auto,1";
   env = [
     "GDK_SCALE,1"
+    "WLR_DRM_NO_ATOMIC,1"
+  ];
+  windowrule = [
+    "immediate, ^(Minecraft*)$"
   ];
   };
 home.file.".config/hypr/hyprpaper.conf".source = ../dotfiles/hypr/hyprpaper-desktop.conf;
