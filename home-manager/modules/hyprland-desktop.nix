9b6fde77dacc8ed88acb2ea785018ca2ddf18612
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-04-29 13:26:31 +0100

    (rebase) hypr: allow tearing on desktop

--- home-manager/modules/hyprland-desktop.nix
+++ home-manager/modules/hyprland-desktop.nix
@@ -6,9 +6,15 @@ wayland.windowManager.hyprland.settings = {
     "GDK_SCALE,1"
     "WLR_DRM_NO_ATOMIC,1"
   ];
-  windowrule = [
-    "immediate, ^(Minecraft*)$"
+  windowrulev2 = [
+    "immediate, class:^(Minecraft*)$"
+    "immediate, class:^(valheim.x86_64)$"
+    "idleinhibit focus, class:^(steam_app*)$"
+    "immediate, class:^(steam)$"
   ];
+  general = {
+    allow_tearing = true;
+  };
   };
 home.file.".config/hypr/hyprpaper.conf".source = ../dotfiles/hypr/hyprpaper-desktop.conf;
 }
