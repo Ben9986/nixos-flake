873859559f70bf73481ae519422716e8155d4c49
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-04-29 13:27:04 +0100

    (rebase) hypr: add pin keybidn

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -99,181 +99,179 @@ in {
           "fade, 1, 7, default"
           "workspaces, 1, 2, newBezier"
         ];
-    };
-    
-    dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-        force_split = 2;
-        no_gaps_when_only = true;
-        special_scale_factor = 0.95;
-    };
-    
-    master."new_is_master" = true;
-    
-    
-    gestures = {
-        "workspace_swipe" = true;
-        "workspace_swipe_invert" = false;
-        "workspace_swipe_distance" = 350;
-        "workspace_swipe_min_speed_to_force" = 20;
-        "workspace_swipe_cancel_ratio" = 0.2;
-    };
-    
-    misc = {
-        vrr = 1;
-        "key_press_enables_dpms" = true;
-        "enable_swallow" = true;
-        "swallow_regex" = "^(kitty)|()$";
-        "swallow_exception_regex" = "^(firefox)|(nemo)$";
-        "focus_on_activate" = true;
-        "disable_hyprland_logo" = true;
-        "force_default_wallpaper" = false;
-    };
-    
-    xwayland = {
-      force_zero_scaling = true;
-    };
-    
-    windowrule = [
-      "float, ^(blueberry.py)$"
-    "size 350 265, ^(blueberry.py)$"
-    "move onscreen cursor 70% 5%, ^(blueberry.py)$"
-    "noanim, ^(blueberry.py)$"
-    
-    "float, ^(peazip)$"
-    
-    "noblur, ^(nemo)$"
-    "noblur, ^(com.obsproject.Studio)$"
-    "opaque, ^(com.obsproject.Studio)$"
-    "opaque, ^(io.github.alainm23.planify)$"
-    "noblur, ^(io.github.alainm23.planify)$"
-    
-    "noborder, ^(wofi)$"
-    "noanim, ^(wlogout)$"
-    "float, ^(wlogout)$"
-    "fullscreen, ^(wlogout)$"
-    "opacity 0.8 override 0.8 override, ^(kitty)$"
-    "float, ^(com.github.hluk.copyq)"
-    "size 40% 60%, ^(com.github.hluk.copyq)"
-    "center, ^(com.github.hluk.copyq)"
-    "tile, ^(ONLYOFFICE Desktop Editors)"
-    "float, ^(xdg-desktop-portal-gtk)"
-    "size 50% 60%, ^(xdg-desktop-portal-gtk)"
-    "idleinhibit focus, ^(steam_app*)"
-    
-    "size 600 600, title:(.*)(Bitwarden)$"
-    ];
-    
-    "$mainMod" = "SUPER";
-    
-    bind = [
-    
-    # General Window Control
-    "$mainMod, C, killactive" 
-    "$mainMod, P, togglefloating" 
-    "$mainMod, J, togglesplit" # dwindle
-    "$mainMod SHIFT, F, fullscreen, 1" # maximise
-    "$mainMod CTRL, F, fakefullscreen" # window thinks it's fullscreen
-    "$mainMod ALT, F, fullscreen, 0" # actual fullscreen
-    
-    "$mainMod, K, togglegroup"
-    "$mainMod, M, changegroupactive"
-    
-    "$mainMod, B, exec, pypr expose"
-    
-    # Session Control
-    "$mainMod ALT, P, exec, ${inputs.ags.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/ags -t powermenu"
-    "$mainMod, L, exec, loginctl lock-session"
-    "$mainMod, I, exec, matcha -t && notify-send 'Toggled Idle Inhibitor'"
-    
-    # App Launch Shortcuts
-    "$mainMod, Q, exec, kitty"
-    "$mainMod, F, exec, firefox"
-    "SUPER, V, exec, copyq toggle"
-    "$mainMod, N, exec, swaync-client -t -sw"
-    "$mainMod, O, exec, flatpak run --user md.obsidian.Obsidian"
-    "$mainMod, T, exec, io.github.alainm23.planify.quick-add"
-    "$mainMod, D, exec, flatpak run dev.vencord.Vesktop"
-    
-    
-    #F-keys shortcuts
-    ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-    ", kbbrightcycle, exec, ~/.local/bin/backlightcontrol.sh"
-    # kill in the bind below doesn't work :/
-    "$mainMod SHIFT, S, exec, kill -9 $(pidof hyprshot) || XCURSOR_SIZE=32 HYPRSHOT_DIR=$HOME/Pictures/Screenshots ${pkgs.hyprshot}/bin/hyprshot -m region"
-    #", XF86Launch1, exec, ~/.local/bin/gtk-dark-light-toggle.sh # Toggle gnome dark/light mode"
-    
-    # Move focus with mainMod + arrow keys
-    "$mainMod, left, movefocus, l"
-    "$mainMod, right, movefocus, r"
-    "$mainMod, up, movefocus, u"
-    "$mainMod, down, movefocus, d"
-    
-    # Move windows with SUPER + ALT + arrow keys
-    "$mainMod ALT, left, swapwindow, l"
-    "$mainMod ALT, right, swapwindow, r"
-    "$mainMod ALT, up, swapwindow, u"
-    "$mainMod ALT, down, swapwindow, d"
-    
-    # Switch workspaces with mainMod + [0-9]
-    "$mainMod, W, togglespecialworkspace, fm"
-    "$mainMod, 1, workspace, 1"
-    "$mainMod, 2, workspace, 2"
-    "$mainMod, 3, workspace, 3"
-    "$mainMod, 4, workspace, 4"
-    "$mainMod, 5, workspace, 5"
-    "$mainMod, 6, workspace, 6"
-    "$mainMod, 7, workspace, 7"
-    "$mainMod, 8, workspace, 8"
-    "$mainMod, 9, workspace, 9"
-    "$mainMod, 0, workspace, 10"
-    
-    # Move active window to a workspace with mainMod + SHIFT + [0-9]
-    "$mainMod SHIFT, W, movetoworkspace, special:fm"
-    "$mainMod SHIFT, 1, movetoworkspace, 1"
-    "$mainMod SHIFT, 2, movetoworkspace, 2"
-    "$mainMod SHIFT, 3, movetoworkspace, 3"
-    "$mainMod SHIFT, 4, movetoworkspace, 4"
-    "$mainMod SHIFT, 5, movetoworkspace, 5"
-    "$mainMod SHIFT, 6, movetoworkspace, 6"
-    "$mainMod SHIFT, 7, movetoworkspace, 7"
-    "$mainMod SHIFT, 8, movetoworkspace, 8"
-    "$mainMod SHIFT, 9, movetoworkspace, 9"
-    "$mainMod SHIFT, 0, movetoworkspace, 10"
-    
-    # Open numbered special workspaces
-    "ALT, 1, togglespecialworkspace, 1"
-    "ALT, 2, togglespecialworkspace, 2"
-    "ALT, 3, togglespecialworkspace, 3"
-    "ALT, 4, togglespecialworkspace, 4"
-    "ALT, 5, togglespecialworkspace, 5"
-    
-    # Move active window to numbered special workspace
-    "ALT SHIFT, 1, movetoworkspace, special:1"
-    "ALT SHIFT, 2, movetoworkspace, special:2"
-    "ALT SHIFT, 3, movetoworkspace, special:3"
-    "ALT SHIFT, 4, movetoworkspace, special:4"
-    "ALT SHIFT, 5, movetoworkspace, special:5"
-    ];
-    bindr = [
-    #"SUPER, SUPER_L , exec, pkill wofi || GTK_THEME=Catppuccin-Mocha-Standard-Blue-Dark wofi"
-    "SUPER, SUPER_L , exec, pkill fuzzel || fuzzel --fuzzy-min-length=4"
-    ];
-    bindm = [
-    # Move/resize windows with mainMod + LMB/RMB and dragging
-    "$mainMod, mouse:272, movewindow" # LMB
-    "$mainMod, mouse:274, resizewindow" # RMB
-    ];
-    binde = [
-    ", XF86AudioLowerVolume, exec, ags -r \"indicator.popup(1)\"; wpctl set-volume -l 1.25 @DEFAULT_AUDIO_SINK@ 5%-"
-    ", XF86AudioRaiseVolume, exec, ags -r \"indicator.popup(1)\"; wpctl set-volume -l 1.25 @DEFAULT_AUDIO_SINK@ 5%+"
-    ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-    ", XF86MonBrightnessUp, exec, brightnessctl --min-value=20 s 40+"
-    ", XF86MonBrightnessDown, exec, brightnessctl --min-value=20 s 40-"
-    
-    ];
-    
-    };
-  };
+};
+
+dwindle = {
+    pseudotile = true;
+    preserve_split = true;
+    force_split = 2;
+    no_gaps_when_only = true;
+    special_scale_factor = 0.95;
+};
+
+master."new_is_master" = true;
+
+
+gestures = {
+    "workspace_swipe" = true;
+    "workspace_swipe_invert" = false;
+    "workspace_swipe_distance" = 350;
+    "workspace_swipe_min_speed_to_force" = 20;
+    "workspace_swipe_cancel_ratio" = 0.2;
+};
+
+misc = {
+    vrr = 1;
+    "key_press_enables_dpms" = true;
+    "enable_swallow" = true;
+    "swallow_regex" = "^(kitty)|()$";
+    "swallow_exception_regex" = "^(firefox)|(nemo)$";
+    "focus_on_activate" = true;
+    "disable_hyprland_logo" = true;
+    "force_default_wallpaper" = false;
+};
+
+xwayland = {
+  force_zero_scaling = true;
+};
+
+windowrule = [
+  "float, ^(blueberry.py)$"
+"size 350 265, ^(blueberry.py)$"
+"move onscreen cursor 70% 5%, ^(blueberry.py)$"
+"noanim, ^(blueberry.py)$"
+
+"float, ^(peazip)$"
+
+"noblur, ^(nemo)$"
+"noblur, ^(com.obsproject.Studio)$"
+"opaque, ^(com.obsproject.Studio)$"
+"opaque, ^(io.github.alainm23.planify)$"
+"noblur, ^(io.github.alainm23.planify)$"
+
+"noborder, ^(wofi)$"
+"noanim, ^(wlogout)$"
+"float, ^(wlogout)$"
+"fullscreen, ^(wlogout)$"
+"opacity 0.8 override 0.8 override, ^(kitty)$"
+"float, ^(com.github.hluk.copyq)"
+"size 40% 60%, ^(com.github.hluk.copyq)"
+"center, ^(com.github.hluk.copyq)"
+"tile, ^(ONLYOFFICE Desktop Editors)"
+"float, ^(xdg-desktop-portal-gtk)"
+"size 50% 60%, ^(xdg-desktop-portal-gtk)"
+
+"size 600 600, title:(.*)(Bitwarden)$"
+];
+
+"$mainMod" = "SUPER";
+
+bind = [
+
+# General Window Control
+"$mainMod, C, killactive" 
+"$mainMod, P, togglefloating" 
+"$mainMod, B, pin, active"
+"$mainMod, J, togglesplit" # dwindle
+"$mainMod SHIFT, F, fullscreen, 1" # maximise
+"$mainMod CTRL, F, fakefullscreen" # window thinks it's fullscreen
+"$mainMod ALT, F, fullscreen, 0" # actual fullscreen
+
+"$mainMod, K, togglegroup"
+"$mainMod, M, changegroupactive"
+
+"$mainMod, B, exec, pypr expose"
+
+# Session Control
+"$mainMod ALT, P, exec, ${inputs.ags.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/ags -t powermenu"
+"$mainMod, L, exec, loginctl lock-session"
+"$mainMod, I, exec, matcha -t && notify-send 'Toggled Idle Inhibitor'"
+
+# App Launch Shortcuts
+"$mainMod, Q, exec, kitty"
+"$mainMod, F, exec, firefox"
+"SUPER, V, exec, copyq toggle"
+"$mainMod, N, exec, swaync-client -t -sw"
+"$mainMod, O, exec, flatpak run --user md.obsidian.Obsidian"
+"$mainMod, T, exec, io.github.alainm23.planify.quick-add"
+"$mainMod, D, exec, flatpak run dev.vencord.Vesktop"
+
+
+#F-keys shortcuts
+", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+", kbbrightcycle, exec, ~/.local/bin/backlightcontrol.sh"
+# kill in the bind below doesn't work :/
+"$mainMod SHIFT, S, exec, kill -9 $(pidof hyprshot) || XCURSOR_SIZE=32 HYPRSHOT_DIR=$HOME/Pictures/Screenshots ${pkgs.hyprshot}/bin/hyprshot -m region"
+#", XF86Launch1, exec, ~/.local/bin/gtk-dark-light-toggle.sh # Toggle gnome dark/light mode"
+
+# Move focus with mainMod + arrow keys
+"$mainMod, left, movefocus, l"
+"$mainMod, right, movefocus, r"
+"$mainMod, up, movefocus, u"
+"$mainMod, down, movefocus, d"
+
+# Move windows with SUPER + ALT + arrow keys
+"$mainMod ALT, left, swapwindow, l"
+"$mainMod ALT, right, swapwindow, r"
+"$mainMod ALT, up, swapwindow, u"
+"$mainMod ALT, down, swapwindow, d"
+
+# Switch workspaces with mainMod + [0-9]
+"$mainMod, W, togglespecialworkspace, fm"
+"$mainMod, 1, workspace, 1"
+"$mainMod, 2, workspace, 2"
+"$mainMod, 3, workspace, 3"
+"$mainMod, 4, workspace, 4"
+"$mainMod, 5, workspace, 5"
+"$mainMod, 6, workspace, 6"
+"$mainMod, 7, workspace, 7"
+"$mainMod, 8, workspace, 8"
+"$mainMod, 9, workspace, 9"
+"$mainMod, 0, workspace, 10"
+
+# Move active window to a workspace with mainMod + SHIFT + [0-9]
+"$mainMod SHIFT, W, movetoworkspace, special:fm"
+"$mainMod SHIFT, 1, movetoworkspace, 1"
+"$mainMod SHIFT, 2, movetoworkspace, 2"
+"$mainMod SHIFT, 3, movetoworkspace, 3"
+"$mainMod SHIFT, 4, movetoworkspace, 4"
+"$mainMod SHIFT, 5, movetoworkspace, 5"
+"$mainMod SHIFT, 6, movetoworkspace, 6"
+"$mainMod SHIFT, 7, movetoworkspace, 7"
+"$mainMod SHIFT, 8, movetoworkspace, 8"
+"$mainMod SHIFT, 9, movetoworkspace, 9"
+"$mainMod SHIFT, 0, movetoworkspace, 10"
+
+# Open numbered special workspaces
+"ALT, 1, togglespecialworkspace, 1"
+"ALT, 2, togglespecialworkspace, 2"
+"ALT, 3, togglespecialworkspace, 3"
+"ALT, 4, togglespecialworkspace, 4"
+"ALT, 5, togglespecialworkspace, 5"
+
+# Move active window to numbered special workspace
+"ALT SHIFT, 1, movetoworkspace, special:1"
+"ALT SHIFT, 2, movetoworkspace, special:2"
+"ALT SHIFT, 3, movetoworkspace, special:3"
+"ALT SHIFT, 4, movetoworkspace, special:4"
+"ALT SHIFT, 5, movetoworkspace, special:5"
+];
+bindr = [
+#"SUPER, SUPER_L , exec, pkill wofi || GTK_THEME=Catppuccin-Mocha-Standard-Blue-Dark wofi"
+"SUPER, SUPER_L , exec, pkill fuzzel || fuzzel --fuzzy-min-length=4"
+];
+bindm = [
+# Move/resize windows with mainMod + LMB/RMB and dragging
+"$mainMod, mouse:272, movewindow" # LMB
+"$mainMod, mouse:274, resizewindow" # RMB
+];
+binde = [
+", XF86AudioLowerVolume, exec, ags -r \"indicator.popup(1)\"; wpctl set-volume -l 1.25 @DEFAULT_AUDIO_SINK@ 5%-"
+", XF86AudioRaiseVolume, exec, ags -r \"indicator.popup(1)\"; wpctl set-volume -l 1.25 @DEFAULT_AUDIO_SINK@ 5%+"
+", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+", XF86MonBrightnessUp, exec, brightnessctl --min-value=20 s 40+"
+", XF86MonBrightnessDown, exec, brightnessctl --min-value=20 s 40-"
+
+];
+};
 }
