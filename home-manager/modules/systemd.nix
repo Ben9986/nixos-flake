9dc175d2417105e72ac013832ea2c790c5c249a3
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-05-06 00:30:01 +0100

    hm: add onedrive services

--- home-manager/modules/systemd.nix
+++ home-manager/modules/systemd.nix
@@ -10,7 +10,8 @@
     };
   };
  
-  systemd.user.services."pull-flake" = {
+  systemd.user.services = {
+  "pull-flake" = {
      Unit = {
       Description = "Pull changes for flake configuration from github";
     };
@@ -27,4 +28,42 @@
       Type = "oneshot";
       };
   };
+
+  "rclone-strath" = {
+    Unit = {
+      Description = "rclone: Remote FUSE filesystem for cloud storage config %i";
+      After = "network-online.target";
+      Wants = "network-online.target";
+    };
+    Install = {
+      WantedBy= [ "default.target" ];
+    };
+    Service = {
+      Type = "notify";
+      ExecStart = ''
+  ${pkgs.rclone}/bin/rclone mount --config=%h/.config/rclone/rclone.conf --vfs-cache-mode full OneDrive-Strathclyde: %h/OneDrive-Strathclyde
+      '';
+      ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/OneDrive-Strathclyde";
+    };
+  };
+
+  "rclone-personal" = {
+    Unit = {
+      Description = "rclone: Remote FUSE filesystem for cloud storage config %i";
+      After = "network-online.target";
+      Wants = "network-online.target";
+    };
+    Install = {
+      WantedBy= [ "default.target" ];
+    };
+    Service = {
+      Type = "notify";
+      ExecStart = ''
+  ${pkgs.rclone}/bin/rclone mount --config=%h/.config/rclone/rclone.conf --vfs-cache-mode full OneDrive-Personal: %h/OneDrive-Personal
+      '';
+      ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/OneDrive-Personal";
+    };
+  };
+};
+
 }
