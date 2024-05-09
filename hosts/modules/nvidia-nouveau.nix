51264ee22643a02bb322ade07638736ba58dce18
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-05-10 00:08:30 +0100

    nvidia: tweak nouveau option

--- hosts/modules/nvidia-nouveau.nix
+++ hosts/modules/nvidia-nouveau.nix
@@ -7,6 +7,7 @@
       driSupport = true;
       driSupport32Bit = true;
     };
-  kernelPackages = pkgs.linuxPackages_6_8;
+    # zen kernel is already on 6.8 so not needed
+  # kernelPackages = lib.mkForce pkgs.linuxPackages_6_8;
   boot.kernelParams = [ "nouveau.config=NvGspRM=1" ];
 }
