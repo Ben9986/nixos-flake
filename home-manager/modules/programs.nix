7d87379e20e4c231f31601f6c7296532877e85bb
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-06-25 00:57:29 +0100

    amend 94f341b. missing programs.nix

--- home-manager/modules/programs.nix
+++ home-manager/modules/programs.nix
@@ -8,7 +8,39 @@ programs = {
   };
   firefox.enable = true;
   gh.enable = true;
-  yazi.enable = true;
+  kitty = {
+    enable = true;
+    theme = "Cherry Midnight";
+    settings = {
+      enable_audio_bell = false;
+      editor = "nvim";
+      confirm_os_window_close = -1;
+      shell_integration = true;
+    };
+    };
+  neovim = {
+    defaultEditor = true;
+    plugins = [
+      pkgs.vimPlugins.yuck-vim
+    ];
+  };
+  yazi = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      manager = {
+        ratio = [1 3 3];
+        show_hidden = true;
+        show_simlink = true;
+        sort_by = "natural";
+        sort_dir_first = true;
+      };
+    };
+  };
+  zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 };
 
 }
