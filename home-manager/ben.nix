94f341b1eb0fa20d1fdf22ae984d80ca75aac6ed
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-06-25 00:56:13 +0100

    hm: move programs to programs.nix

--- home-manager/ben.nix
+++ home-manager/ben.nix
@@ -46,44 +46,6 @@
         accountsservice
       ];
     };
-
-    neovim = {
-      defaultEditor = true;
-      plugins = [
-        pkgs.vimPlugins.yuck-vim
-      ];
-    };
-  
-    kitty = {
-      enable = true;
-      theme = "Cherry Midnight";
-      settings = {
-        enable_audio_bell = false;
-        editor = "nvim";
-        confirm_os_window_close = -1;
-        shell_integration = true;
-      };
-    };
-  
-    yazi = {
-      enable = true;
-      enableZshIntegration = true;
-      settings = {
-        manager = {
-	        ratio = [1 3 3];
-	        show_hidden = true;
-	        show_simlink = true;
-	        sort_by = "natural";
-	        sort_dir_first = true;
-        };
-      };
-    };
-  
-    zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
     home-manager.enable = true;
   };
 }
