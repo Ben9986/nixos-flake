1cb7d63a6e9581088ec438e8eeb3d86b24861584
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-06-25 01:08:44 +0100

    desktop: only enable blueman on hyprland

--- hosts/bendesktop/default.nix
+++ hosts/bendesktop/default.nix
@@ -87,7 +87,7 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
-  services.blueman.enable = true;
+  services.blueman.enable = lib.mkIf config.hyprland.enable true;
 
   environment.systemPackages = with pkgs; [
     mangohud
