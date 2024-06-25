35f100c2ee0af6b9a8c3676b62982f9c5538153d
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-06-25 01:06:59 +0100

    hm: remove unused program configs

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -5,12 +5,9 @@ imports = [
   ./hypridle.nix
   ./fuzzel.nix
   ./programs.nix
-  ./swayidle.nix
   ./swaylock.nix
   ./systemd.nix
   ./theming.nix
-  ./waybar.nix
-  ./wofi.nix
   ./xdg.nix
   ./zsh.nix
 ];
