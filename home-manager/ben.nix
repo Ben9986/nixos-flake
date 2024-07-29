ccbcf34d6589bcbaf9e56c00447e562d8453ad99
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2024-07-29 23:48:33 +0100

    hm: use custom opt to enable hypr config

--- home-manager/ben.nix
+++ home-manager/ben.nix
@@ -4,38 +4,41 @@
   imports = [ 
     inputs.ags.homeManagerModules.default
     ];
+  config = {
+      custom.hyprland.enable = false;
 
-  home = {
-    username = "ben";
-    homeDirectory = "/home/ben";
-    stateVersion = "23.05"; # Please read the comment before changing.
-    
-    sessionVariables = {
-      EDITOR = "nvim";
-      VISIAL = "nvim";
-      PAGER = "less";
-      PATH = "$HOME/.nix-profile/bin:$HOME/.local/bin:$PATH";
-      RANGER_LOAD_DEFAULT_RC="false";
+    home = {
+      username = "ben";
+      homeDirectory = "/home/ben";
+      stateVersion = "23.05"; # Please read the comment before changing.
+      
+      sessionVariables = {
+        EDITOR = "nvim";
+        VISIAL = "nvim";
+        PAGER = "less";
+        PATH = "$HOME/.nix-profile/bin:$HOME/.local/bin:$PATH";
+        RANGER_LOAD_DEFAULT_RC="false";
+      };
+      packages = with pkgs; [
+        (nerdfonts.override { fonts = [ "RobotoMono" "JetBrainsMono" "SpaceMono" "Ubuntu"]; })
+        phinger-cursors
+        neovim
+        wget
+        rclone
+        libnotify
+        cantarell-fonts
+        roboto
+        git-crypt
+        pavucontrol
+        spotify
+        floorp
+      ];
+      
     };
-    packages = with pkgs; [
-      (nerdfonts.override { fonts = [ "RobotoMono" "JetBrainsMono" "SpaceMono" "Ubuntu"]; })
-      phinger-cursors
-      neovim
-      wget
-      rclone
-      libnotify
-      cantarell-fonts
-      roboto
-      git-crypt
-      pavucontrol
-      spotify
-      floorp
-    ];
-    
+    nixpkgs.config.allowUnfree = true;
+
+    services.easyeffects.enable = true;
+
+    programs.home-manager.enable = true;
   };
-  nixpkgs.config.allowUnfree = true;
-
-  services.easyeffects.enable = true;
-
-  programs.home-manager.enable = true;
 }
