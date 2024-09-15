edf0f4f38ef5f8aaa6fddb0fa303fa4383ff0603
ben9986 <38633150+Ben9986@users.noreply.github.com>
2024-09-15 21:42:35 +0100

    pkgs: klassy rewrite

--- pkgs/klassy.nix
+++ pkgs/klassy.nix
@@ -1,44 +1,58 @@
-{
-  stdenv,
-  fetchFromGitHub,
-  pkgs,
-  lib,
-}:
-stdenv.mkDerivation rec {
+{ lib, pkgs }:
+pkgs.stdenv.mkDerivation rec {
   pname = "klassy";
   version = "6.1.breeze6.0.3";
-  dontWrapQtApps = true;
-  nativeBuildInputs =
-    with pkgs.kdePackages;
-    [
-      extra-cmake-modules
-      qtbase
-      kcmutils
-      kcolorscheme
-      kconfig
-      kconfigwidgets
-      kcoreaddons
-      kguiaddons
-      ki18n
-      kiconthemes
-      kirigami
-      kpackage
-      kservice
-      kwindowsystem
-      kwayland
-      frameworkintegration
-      kdecoration
-      qtsvg
-    ]
-    ++ [
-      pkgs.cmake
-      pkgs.xorg.libX11
-    ];
-  cmakeFlags = [ "-DBUILD_QT5=OFF" ];
-  src = fetchFromGitHub {
+
+  src = pkgs.fetchFromGitHub {
     owner = "paulmcauley";
     repo = "klassy";
-    rev = version;
+    rev = "refs/tags/${version}";
     hash = "sha256-D8vjc8LT+pn6Qzn9cSRL/TihrLZN4Y+M3YiNLPrrREc=";
+  };
+
+  dontWrapQtApps = true;
+
+  cmakeFlags = [ "-DBUILD_QT5=OFF" ];
+
+  nativeBuildInputs = with pkgs; [
+    kdePackages.extra-cmake-modules
+    cmake
+    xorg.libX11
+  ];
+
+  buildInputs = with pkgs.kdePackages; [
+    qtbase
+    kcmutils
+    kcolorscheme
+    kconfig
+    kconfigwidgets
+    kcoreaddons
+    kguiaddons
+    ki18n
+    kiconthemes
+    kirigami
+    kpackage
+    kservice
+    kwindowsystem
+    kwayland
+    frameworkintegration
+    kdecoration
+    qtsvg
+  ];
+
+  meta = {
+    description = "Highly customizable Window Decorations and Application Style plugin for Plasma 6";
+    homepage = "https://github.com/paulmcauley/klassy";
+    license = with lib.licenses; [
+      bsd3
+      cc0
+      gpl2Only
+      gpl2Plus
+      gpl3Only
+      gpl3Plus
+      mit
+    ];
+    maintainers = with lib.maintainers; [ ben9986 ];
+    platforms = lib.platforms.all;
   };
 }
