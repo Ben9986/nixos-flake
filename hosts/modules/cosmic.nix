0fe653c579d1874aca6970cd443df615ae307a8d
ben9986 <38633150+Ben9986@users.noreply.github.com>
2025-01-11 21:47:45 +0000

    cosmic: add kwallet-pam for keyring unlocks

--- hosts/modules/cosmic.nix
+++ hosts/modules/cosmic.nix
@@ -1,5 +1,6 @@
-{ inputs, config, ... }:
+{ inputs, config, pkgs, ... }:
 {
   services.desktopManager.cosmic.enable = config.custom.cosmic.enable;
   services.displayManager.cosmic-greeter.enable = config.custom.cosmic.greeter.enable;
+  environment.systemPackages = with pkgs; [ kwallet-pam];
 }
