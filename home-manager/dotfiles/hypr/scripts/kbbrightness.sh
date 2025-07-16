28ec251dffc4a004d289586369807828d5f36285
ben9986 <38633150+Ben9986@users.noreply.github.com>
2025-07-16 23:31:26 +0100

    hypr: kbdbrightness script changes. (kbbrightcycle key still not functional)

--- home-manager/dotfiles/hypr/scripts/kbbrightness.sh
+++ home-manager/dotfiles/hypr/scripts/kbbrightness.sh
@@ -7,3 +7,16 @@ if [[ "$current" -le "2" ]]; then
 else
 	brightnessctl -d \*::kbd_backlight set 0
 fi
+
+new="$(cat /sys/class/leds/*::kbd_backlight/brightness)"
+osdval=$(echo "scale=2; $new/3" | bc)
+
+echo "$osdval"
+
+if [[ "$osdval" -eq "0" ]]; then
+	osdval=0.001
+fi
+
+swayosd-client --custom-icon keyboard-brightness-symbolic --custom-progress "$osdval"
+
+
