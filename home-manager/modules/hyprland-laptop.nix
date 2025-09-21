27bc550707b5f45fdea4952c8afeaa7c7250d566
ben9986 <38633150+Ben9986@users.noreply.github.com>
2025-09-21 01:14:58 +0100

    hypr: minor typo in hyprpaper config

--- home-manager/modules/hyprland-laptop.nix
+++ home-manager/modules/hyprland-laptop.nix
@@ -19,7 +19,7 @@ lib.mkIf config.custom.hyprland.enable {
       monitor=desc:Acer Technologies Acer V247Y 0x1411579A,1920x1080@74.97,1600x0,1.0,bitdepth,10
     '';
 
-    "config/hypr/monitors-ultrawide.conf".text = ''
+    ".config/hypr/monitors-ultrawide.conf".text = ''
       monitor=desc:Samsung Display Corp. 0x4171,2880x1800@90.0,515x1080,1.8,bitdepth,10
       monitor=desc:LG Electronics LG ULTRAWIDE 0x000542FD,2560x1080@60.0,0x0,1.0,bitdepth,10
     '';
