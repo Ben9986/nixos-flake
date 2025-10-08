d96d11a1cc2d69fd51b9979588e5fdfbf1688ef1
ben9986 <38633150+Ben9986@users.noreply.github.com>
2025-10-08 20:46:37 +0100

    plasma: option to enable/disable sddm config

--- hosts/modules/plasma.nix
+++ hosts/modules/plasma.nix
@@ -3,10 +3,17 @@ let cfg = config.plasma;
 in {
   options.plasma = {
     enable = lib.mkEnableOption "Plasma Desktop Sessoion";
+    sddm.enable = lib.mkEnableOption "SDDM Display Manager";
   };
 
   config = lib.mkIf cfg.enable {
     services = {
+      # sddm needs plasma-workspace for breeze theme otherwise fallback theme
+      displayManager.sddm = {
+        enable = cfg.sddm.enable;
+        wayland.enable = true;
+        theme = "breeze";
+      };
       desktopManager.plasma6.enable = true;
     };
 };
