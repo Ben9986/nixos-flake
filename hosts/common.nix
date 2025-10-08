9f354f23d942e5ab4b18a9d54953479d9f761d69
ben9986 <38633150+Ben9986@users.noreply.github.com>
2025-10-08 22:49:34 +0100

    remove cosmic cachix

--- hosts/common.nix
+++ hosts/common.nix
@@ -84,11 +84,9 @@ in
       trusted-substituters = [ "https://hyprland.cachix.org" ];
       substituters = [
         "https://hyprland.cachix.org"
-        "https://cosmic.cachix.org/"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       ];
     };
     package = pkgs.nixVersions.stable;
