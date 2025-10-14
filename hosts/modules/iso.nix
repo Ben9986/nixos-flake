05261dc5cf413f29106230c2a9d9f07438da715a
ben9986 <38633150+Ben9986@users.noreply.github.com>
2025-10-14 23:07:01 +0100

    iso: add custom scripts btrfs mount script install from flake script add memtest86 set correct keyboard layout

--- hosts/modules/iso.nix
+++ hosts/modules/iso.nix
@@ -1,13 +1,62 @@
 {pkgs, lib, config, ... }:
-let custom-installer = pkgs.writeShellScriptBin "custom-installer" ''
-  echo "Hello, world!"
+let 
+fsmount = pkgs.writeShellScriptBin "fsmount" ''
+  #!${lib.getExe pkgs.bash}
+
+  commands=(
+    "sudo mount -o compress=zstd,subvol=/ /dev/nvme0n1p6 /mnt"
+    "sudo mount -o compress=zstd,subvol=/home /dev/nvme0n1p6 /mnt/home"
+    "sudo mount -o compress=zstd,noatime,subvol=/nix /dev/nvme0n1p6 /mnt/nix"
+    "sudo mount /dev/nvme0n1p5 /mnt/boot"
+  )
+
+  for cmd in "''${commands[@]}"; do
+    echo
+    echo "About to run: $cmd"
+    read -p "Proceed? (y/n) " confirm
+
+    if [[ "$confirm" =~ ^[Yy]$ ]]; then
+      eval "$cmd"
+    else
+      echo "Skipped."
+    fi
+  done
+  '';
+
+  quick-install = pkgs.writeShellScriptBin "custom-reinstall" ''
+    #!${lib.getExe pkgs.bash}
+
+    echo "Checking for internet connection"
+    if ping 1.1.1.1 -q -c 1 -W 2; then
+      echo "Connection detected"
+    else
+      echo "No connection detected!\n Use nmtui to connect to a network"
+      exit 1
+    fi
+
+    echo "Cloning flake config..."
+    flakedir=/tmp/flake-config
+    git clone https://github.com/ben9986/nixos-flake $flakedir && echo "Clone successful"
+
+    read -p "Confirm hostname to be installed (benlaptop/bendesktop/trinity) " hostname
+
+    cmd="sudo nixos-install --flake /home/nixos/flake-config#$hostname --root /mnt --no-root-password"
+    
+    echo "About to run: $cmd"
+    read -p "Proceed? (y/n) " confirm 
+
+    if [[ "$confirm" =~ ^[Yy]$ ]]; then
+      eval "$cmd"
+    else
+      echo "Skipped."
+    fi
   '';
   cfg = config.iso;
 in
 {
   options.iso.enable = lib.mkEnableOption "Custom ISO Configuration";
 
-  config = lib.mkIf cfg.iso {
+  config = lib.mkIf cfg.enable {
     isoImage = {
       makeEfiBootable = true;
       makeUsbBootable = true;
@@ -15,10 +64,14 @@ in
       edition = "Custom";
       contents = [ { source = ../../.; target = "/flake-config"; } ];
     }; 
+    boot.loader.grub.memtest86.enable = true;
+
+    i18n.defaultLocale = "en_GB.UTF-8";
     networking.networkmanager.enable = true;
      environment.systemPackages = with pkgs; [
       git
-      custom-installer
+      fsmount
+      quick-install
      ];
   };
 }
